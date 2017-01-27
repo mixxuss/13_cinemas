e1c83b76d622856b07b82a4bbbad790af5c509e2
mixxuss <mixxuss@gmail.com>
2017-01-27 17:11:37 +0300

    Added fetch_afisha_page and parse_afisha_list functions

--- cinemas.py
+++ cinemas.py
@@ -1,11 +1,21 @@
-
+import requests
+from bs4 import BeautifulSoup
 
 def fetch_afisha_page():
-    pass
+    html = requests.get('http://www.afisha.ru/msk/schedule_cinema/')
+    html_page = html.text
+    return html_page
 
 
 def parse_afisha_list(raw_html):
-    pass
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    page_elements = soup.find_all('div', {'class': 'object s-votes-hover-area collapsed'})
+    list_movies = []
+    for element in page_elements:
+        tuple_movie_places = (element.find('div', {'class': 'm-disp-table'}).find('a').text,
+                              len(element.find_all('tr')))
+        list_movies.append(tuple_movie_places)
+    return list_movies
 
 
 def fetch_movie_info(movie_title):
@@ -17,4 +27,35 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    # raw_html = fetch_afisha_page()
+    # list_movie_places = (parse_afisha_list(raw_html))
+    kinopoisk_page = requests.get('https://www.kinopoisk.ru/s/type/all/find/Балерина')#, 'Балерина')
+    html = kinopoisk_page.text
+    #ele = html.find('div', {'class': 'rating'})
+    print(html)#.find('div', {'class': 'element most_wanted'}))
+
+
+
+
+
+
+
+    # movie_list = Movie.objects.search('Балерина')
+    # print(movie_list[0].title)
+    # print(movie_list[0].id)
+    # movie = Movie(movie_list[0].id)
+    # movie.get_content('main_page')
+    # print(movie)
+
+
+
+
+
+
+
+    # soup = BeautifulSoup(html_page, 'html.parser')
+    # elements = soup.find_all('div', {'class': 'object s-votes-hover-area collapsed'})
+    # print(elements)
+    # for element in elements:
+    #    print(element.find('div', {'class': 'm-disp-table'}).find('a').text)
+    #    print(len(element.find_all('tr')))
