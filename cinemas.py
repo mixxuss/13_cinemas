ec777a4923fe0c04634f2d56a58f81bbd3a477e9
mixxuss <mixxuss@gmail.com>
2017-01-29 19:50:05 +0300

    Added cookies support

--- cinemas.py
+++ cinemas.py
@@ -31,7 +31,7 @@ def make_dict_name_rating(movie_names_list):
     names_rating_votes_dict = {}
     for movie_name in movie_names_list:
         names_rating_votes_dict[movie_name] = fetch_movie_info(movie_name)
-        time.sleep(10)
+        time.sleep(6)
     return names_rating_votes_dict
 
 
@@ -45,13 +45,23 @@ def fetch_movie_info(movie_name):
         'what': '',
         'kp_query': movie_name
     }
-    response = requests.get(url, params=params, headers=headers, timeout=5.9)
-    search_movie_html = response.text
-    soup = BeautifulSoup(search_movie_html, 'html.parser')
-    element_most_wanted = soup.find_all('div', 'element most_wanted')[0]
-    rating_and_votes_amount_list = element_most_wanted.find_all('div', 'rating ratingGreenBG')[0].attrs['title'].split(' ')
-    tuple_rating_and_votes_amount = list_strings_to_tuple(rating_and_votes_amount_list)
-    return tuple_rating_and_votes_amount
+    cookies = {'user_country': 'ru',
+               'yandex_gid': '10758',
+               'mobile': 'no',
+               'PHPSESSID': '7b74661247009b38f33dee4bfad99827',
+               'last_visit': '2017-01-29+19%3A47%3A43'}
+    try:
+        session = requests.Session()
+        response = session.get(url, params=params, headers=headers, cookies=cookies)
+        search_movie_html = response.text
+        soup = BeautifulSoup(search_movie_html, 'html.parser')
+        element_most_wanted = soup.find_all('div', 'element most_wanted')[0]
+        rating_and_votes_amount_list = element_most_wanted.find_all('div',
+                                                                    'rating ratingGreenBG')[0].attrs['title'].split(' ')
+        tuple_rating_and_votes_amount = list_strings_to_tuple(rating_and_votes_amount_list)
+        return tuple_rating_and_votes_amount
+    except:
+        return (0, 0)
 
 
 def list_strings_to_tuple(list_rating):
@@ -67,11 +77,9 @@ def make_top_list(name_rating_dict, results_amount):
     return sorted_list[0:int(results_amount)-1]
 
 
-
 def output_to_console(sorted_movie_list):
     for movie in sorted_movie_list:
         print('Movie %s had %s average rating (%s votes)' % (movie[0], movie[1][0], movie[1][1]))
-
 
 
 if __name__ == '__main__':
@@ -79,8 +87,6 @@ if __name__ == '__main__':
     raw_afisha_html = fetch_afisha_page()
     list_movie_and_places_amount = (parse_afisha_list(raw_afisha_html))
     only_popular_titles_list = get_only_popular_movie_titles(list_movie_and_places_amount)
-    print(only_popular_titles_list)
-    dict_name_rating = make_dict_name_rating(['Притяжение', 'Три икса: Мировое господство', 'Ла-Ла Ленд'])
-    print(dict_name_rating)
-    #top10_list = make_top_list(dict_name_rating, 10)
-    #output_to_console(top10_list)
+    dict_name_rating = make_dict_name_rating(only_popular_titles_list)
+    top10_list = make_top_list(dict_name_rating, top_list_amount)
+    output_to_console(top10_list)
