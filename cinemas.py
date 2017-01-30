b62d256556ffc5061a6b8fb6c4afe71f17079b58
mixxuss <mixxuss@gmail.com>
2017-01-30 19:40:40 +0300

    Deleted unused func Optimized code

--- cinemas.py
+++ cinemas.py
@@ -1,13 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import collections
-
-
-def fetch_afisha_page():
-    html = requests.get('http://www.afisha.ru/msk/schedule_cinema/')
-    html_page = html.text
-    return html_page
 
 
 def parse_afisha_list(raw_html):
@@ -27,7 +20,7 @@ def get_only_popular_movie_titles(dict_movies_places, min_places_amount=30):
 def make_dict_name_rating(movies_names_list):
     names_rating_votes_dict = {}
     for movie_name in movies_names_list:
-        names_rating_votes_dict[movie_name] = fetch_movie_info(fetch_new_kinopoisk_page(fetch_kinopoisk_movie_url(movie_name)))
+        names_rating_votes_dict[movie_name] = fetch_movie_info(get_page_html(fetch_kinopoisk_movie_url(movie_name)))
     return names_rating_votes_dict
 
 
@@ -43,7 +36,7 @@ def fetch_kinopoisk_movie_url(movie_name):
     return movie_url
 
 
-def fetch_new_kinopoisk_page(url):
+def get_page_html(url):
     movie_page_html = requests.get(url).text
     return movie_page_html
 
@@ -63,26 +56,22 @@ def fetch_movie_info(search_movie_html):
 
 
 def make_top_list(name_rating_dict, results_amount):
-    sorted_list = [(movie, name_rating_dict[movie]) for movie in sorted(name_rating_dict,
-                                                                        key=name_rating_dict.get, reverse=True)]
-    if results_amount > len(sorted_list):
-        return sorted_list[0:len(sorted_list)]
-    return sorted_list[0:int(results_amount)-1]
+    name_rating_list = list(name_rating_dict.items())
+    rating = [(movie[0], movie[1]['Rating'], movie[1]['Votes']) for movie in name_rating_list]
+    return sorted(rating, key=lambda movie: movie[1], reverse=True)[0: results_amount]
 
 
 def output_to_console(sorted_movie_list):
     for movie in sorted_movie_list:
-        print('Movie %s had %s average rating (%s votes)' % (movie[0], movie[1][0], movie[1][1]))
+        print('У фильма %s рейтинг %s (%s)' % (movie[0], movie[1], movie[2]))
 
 
 if __name__ == '__main__':
+    afisha_url = 'http://www.afisha.ru/msk/schedule_cinema/'
     top_list_amount = 10
-    # print(fetch_kinopoisk_movie_url('Балерина'))
-    # print(fetch_movie_info(fetch_new_kinopoisk_page(fetch_kinopoisk_movie_url('Балерина'))))
-    raw_afisha_html = fetch_afisha_page()
+    raw_afisha_html = get_page_html(afisha_url)
     list_movie_and_places_amount = (parse_afisha_list(raw_afisha_html))
     only_popular_titles_list = get_only_popular_movie_titles(list_movie_and_places_amount)
     dict_name_rating = make_dict_name_rating(only_popular_titles_list)
-    print(dict_name_rating)
-    #top10_list = make_top_list(dict_name_rating, top_list_amount)
-    #output_to_console(top10_list)
+    top10_list = make_top_list(dict_name_rating, top_list_amount)
+    output_to_console(top10_list)
